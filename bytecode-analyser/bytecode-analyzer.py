d5ce09d9b2e686c9d8b4278a20b2d346ca114cfa
Pedro <nhocamcc@gmail.com>
2018-01-02 15:31:46 -0800

    Tweaks

--- bytecode-analyser/bytecode-analyzer.py
+++ bytecode-analyser/bytecode-analyzer.py
@@ -1,12 +1,11 @@
 import sys, os
-import logging
-import zipfile
-from multiprocessing import Process, Pool
-from javatools import unpack_classfile
-from subprocess import check_output, STDOUT, CalledProcessError
 import json
 import shutil
+import logging
 from zipfile import ZipFile
+from javatools import unpack_classfile
+from multiprocessing import Process, Pool
+from subprocess import check_output, STDOUT, CalledProcessError
 
 # Set:
 # PROJECTS_BUILDS_DIR as builds/ output of JBF
@@ -58,6 +57,7 @@ def run_main(root_path, file_path):
 
 # Finds reachable methods from main
 def reachable_methods_from_main(root_path, file_path, jar_paths):
+    # TODO Add specific files for the output of wiretap to manage concurrent calls
     # Example: 'java -cp .:lava-master/build/: -javaagent:wiretap.jar -Dwiretap.recorder=ReachableMethods com.golaszewski.lava.evaluate.REPL'
     cmd_wiretap = 'java -cp .:%s:%s: -javaagent:wiretap.jar -Dwiretap.recorder=ReachableMethods %s'
 
@@ -146,6 +146,7 @@ def process(list_projs):
 
     proj_counter = 0
 
+    # TODO Make script start from a list of files intead of the results of JBF
     with open(OUTPUT_FILE % pid, 'w') as output_file:
         output_file.write('proj_name,n_class_files,reachable_mains,with_junit,passed_junit\n')
 
